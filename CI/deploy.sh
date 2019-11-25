00055103caafce3eb7dfb6cc8959fb95b7a115d0
bencoder <ben.clark@acuris.com>
2019-11-25 14:24:57 +0000

    run integration tests also

--- CI/deploy.sh
+++ CI/deploy.sh
@@ -9,6 +9,7 @@ fi
 npm ci
 npm run acuris-eslint
 npm test
+npm run test:integration
 npm run build
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
 npm publish --access public
--- CI/release.sh
+++ CI/release.sh
@@ -7,5 +7,6 @@ git checkout master
 git pull
 npm run acuris-eslint
 npm test
+npm run test:integration
 npm version patch -m "Version %s"
 git push --follow-tags
